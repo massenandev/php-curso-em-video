ef125772226dc08d137b5be80e762f9a4c9e7c0b
massenandev <massenandev@gmail.com>
2020-08-20 22:59:37 -0300

    first commit

---
+++ aula04.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="_css/stilo.css">
+    <title>php example</title>
+</head>
+<body>
+    <div>
+        <h1>Testando php</h1>
+            <?php
+                $n = 4;
+                $no = (string)"Gustavo"; // cast entre parentesis pra forçar a conversão. Caso contrário, ele faz coerção
+                echo $no;
+            ?>
+    </div>
+</body>
+</html>
---
+++ aula05.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="_css/stilo.css">
+    <title></title>
+</head>
+<body>
+    <?php
+        $n1 = 3;
+        $n2 = 2;
+        $s = $n1 + $n2;
+        echo "A soma entre $n1 e $n2 é $s."
+    ?>
+</body>
+</html>
