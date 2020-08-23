272d34a96735c19219243af622d999cdcbb620da
massenandev <massenandev@gmail.com>
2020-08-23 00:07:12 -0300

    exercicios

---
+++ aula09/aula09.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" href="_css/stilo.css">
+    <title></title>
+</head>
+<body>
+    <div>
+        <?php
+            $a = isset($_GET["ano"]) ? $_GET["ano"] : 1900;
+            $i = date("Y") - $a;
+            echo "Você nasceu em $a e tem $i anos.";
+            if($i > 16){
+                $v = "já pode votar";
+                $d = "já pode dirigir";
+            }
+            else {
+                $v = "não pode votar ainda";
+                $d = "não pode dirigir";
+            }
+            echo " Portanto, você $v e $d também.";
+        ?>
+    </div>
+</body>
+</html>
